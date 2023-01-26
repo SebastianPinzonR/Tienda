3e65a4ae4bb7ba544a80f34e70c697a2c41fd433
Sebastian Pinzon <spinzonrojas@gmail.com>
2023-01-25 21:42:23 -0500

    Se modificaron las clases vendedor y vendedordao para que funcionara

--- src/tienda/dao/VendedorDao.java
+++ src/tienda/dao/VendedorDao.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tienda.dao;
 
-/**
- *
- * @author SEBASTIANdfgdf
- */
+import tienda.dto.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import tienda.dto.Vendedor;
+
 public class VendedorDao {
+    private Connection conectar;
+    private Object resultSet;
     
+    public boolean create(Vendedor vendedor)throws SQLException {
+        boolean rowCreate =false;
+        try{
+            String sql = "INSERT INTO Productos (idVendedor, Nombre) VALUES(?,?)";
+            conectar = Conexion.Conectar();
+            PreparedStatement statement = (PreparedStatement) conectar.prepareStatement(sql);
+            statement.setInt(1, vendedor.getIdVendedor());
+            statement.setString(2, vendedor.getNombreVendedor());
+            rowCreate = statement.executeUpdate() > 0;
+            statement.close();
+            Conexion.Desconectar();
+            return rowCreate;
+        }   catch (Exception e){
+            System.out.println("error"+e.getMessage());
+        }
+        return rowCreate;
+    }
 }
